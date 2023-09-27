2b0140c11c945a14be65c58907142f23fae12a49
PeterNdeke <ndekepeter2015@gmail.com>
2023-09-28 00:15:18 +0100

    changed route name

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ export default class Server {
 
   constructor() {
     this.server = new InversifyExpressServer(container, null, {
-      rootPath: "/duplo-service",
+      rootPath: "/logs-service",
     });
 
     this.server.setConfig((app01) => {
